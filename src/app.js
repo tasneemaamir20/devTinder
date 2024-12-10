0917f1ed60fbe997a4b6e90861407105ad5ef7c4
tasneemaamir20 <117052484+tasneemaamir20@users.noreply.github.com>
2024-12-10 12:12:00 +0530

    exploring dynamic routing

--- src/app.js
+++ src/app.js
@@ -5,21 +5,30 @@ const app = express();
 // app.use("/user", (req, res) => {
 //   res.send("Hello world from server 2");
 // });
-
+// ! dynamic routing , (\\d+) we can use this for only taking the number input
+app.get("/user/:userId(\\d+)", (req, res) => {
+  console.log(req.params);
+  res.send("succefully run");
+});
+// ! dynamic routing
 app.get("/user/:userId/:name/:password", (req, res) => {
   console.log(req.params);
   res.send({ firstName: "Tasneem ", lastName: "Aamir" });
 });
 
+// ! GET method routing
 app.get("/user", (req, res) => {
   res.send({ firstName: "Tasneem ", lastName: "Aamir" });
 });
+// ! POST method routing
 app.post("/user", (req, res) => {
   res.send("POST Method called succesfully !!!");
 });
+// ! PATCH method routing
 app.patch("/user", (req, res) => {
   res.send("PATCH Method called succesfully !!!");
 });
+// ! DELETE method routing
 app.delete("/user", (req, res) => {
   res.send("DELETE Method called succesfully !!!");
 });
