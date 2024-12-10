0850796232eb4103695f4e8d2b22f766a99648b7
tasneemaamir20 <117052484+tasneemaamir20@users.noreply.github.com>
2024-12-10 18:46:51 +0530

    route handler

--- src/xyz.js
+++ src/xyz.js
@@ -3,7 +3,4 @@ app.use("/user", (req, res) => {
     
     console.log("repsond return to console 1 ");
     res.send("respond");
-}, (req, res) => {
-    console.log("respond return to console 2");
-    res.send("Respond 2");
 });
