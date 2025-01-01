840bedecdc5eb21559d663d29b94d9ecf41d71b4
tasneemaamir20 <117052484+tasneemaamir20@users.noreply.github.com>
2025-01-01 08:52:32 +0530

    Validate schema

--- src/models/user.js
+++ src/models/user.js
@@ -2,51 +2,60 @@ const mongoose = require("mongoose");
 
 const { Schema } = require("mongoose");
 
-const userSchema = new Schema({
-  firstName: {
-    type: String,
-    required: true,
-    minLength: 4,
-    maxLength: 50,
-  },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    minLength: 8,
-  },
-  age: {
-    type: Number,
-    min: 18,
-  },
-  gender: {
-    type: String,
-    validate(value) {
-      if (!["male", "female", "others"].includes(value)) {
-        throw new Error("Gender data is not valid");
-      }
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 50,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      minLength: 8,
+      maxLength: 18,
+    },
+    age: {
+      type: Number,
+      min: 18,
+      max: 35,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender data is not valid");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default: "https://images.app.goo.gl/4Ucf977mAx2nDMiC9",
+    },
+    about: {
+      type: String,
+      default: "This is the default String using default feature",
+      maxLength: 100,
+    },
+    skills: {
+      type: [String],
+      maxLength: [5],
     },
   },
-  photoUrl: {
-    type: String,
-    default: "https://images.app.goo.gl/4Ucf977mAx2nDMiC9",
-  },
-  about: {
-    type: String,
-    default: "This is the default String using default feature",
-  },
-  skills: {
-    type: [String],
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 
